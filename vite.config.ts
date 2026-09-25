0bf3c75391d9a1d5c280c8fe3327cc7e7ae6a297
ashokdorairaj <35213039+ashokdorairaj@users.noreply.github.com>
2026-09-25 12:35:06 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production'
-    ? '/ashokdorairaj/ClaudeProjects/FioriDemos/'
+    ? '/ClaudeProjects/FioriDemos/'
     : '/',
   plugins: [react()],
   resolve: {
